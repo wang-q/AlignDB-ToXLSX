a94315022b3b604f8078835684dd3b675df19931
wang-q <wang-q@outlook.com>
2014-12-04 22:29:29 +0800

    Remove alignDB specific methods.

--- lib/AlignDB/ToXLSX.pm
+++ lib/AlignDB/ToXLSX.pm
@@ -1,21 +1,23 @@
 package AlignDB::ToXLSX;
 
-# ABSTRACT: Generate xlsx files from SQL queries or just arrays. 
+# ABSTRACT: Generate xlsx files from SQL queries or just arrays.
 
 use Moose;
 
 use DBI;
-
-use AlignDB::SQL;
-
 use Excel::Writer::XLSX;
 use List::Util qw(first max maxstr min minstr reduce shuffle sum);
 use List::MoreUtils qw( all any );
+use Statistics::Descriptive;
+
 use YAML qw(Dump Load DumpFile LoadFile);
 
-use Statistics::PointEstimation;
-use Statistics::TTest;
-use Statistics::Descriptive;
+has 'mysql'  => ( is => 'ro', isa => 'Str' );    # e.g. 'alignDB:202.119.43.5'
+has 'server' => ( is => 'ro', isa => 'Str' );    # e.g. '202.119.43.5'
+has 'db'     => ( is => 'ro', isa => 'Str' );    # e.g. 'alignDB'
+has 'user'   => ( is => 'ro', isa => 'Str' );    # database username
+has 'passwd' => ( is => 'ro', isa => 'Str' );    # database password
+has 'dbh'    => ( is => 'ro', isa => 'Ref' );    # store database handle here
 
 has 'outfile'  => ( is => 'ro', isa => 'Str' );     # output file, autogenerable
 has 'workbook' => ( is => 'rw', isa => 'Object' );  # excel workbook object
@@ -24,6 +26,36 @@ has 'columns'  => ( is => 'ro', isa => 'HashRef' ); # excel column names
 
 sub BUILD {
     my $self = shift;
+
+    # Connect to database
+    if ( $self->mysql ) {
+        my ( $server, $db ) = split ':', $self->mysql;
+        $self->{server} ||= $server;
+        $self->{db}     ||= $db;
+    }
+    elsif ( $self->server and $self->db ) {
+        $self->{mysql} = $self->db . ':' . $self->server;
+    }
+    elsif ( $self->mocking ) {
+
+        # do nothing
+    }
+    else {
+        confess "You should provide either mysql or db-server\n";
+    }
+
+    my $mysql  = $self->mysql;
+    my $user   = $self->user;
+    my $passwd = $self->passwd;
+    my $server = $self->server;
+    my $db     = $self->db;
+
+    my $dbh = {};
+    if ( !$self->mocking ) {
+        $dbh = DBI->connect( "dbi:mysql:$mysql", $user, $passwd )
+            or confess "Cannot connect to MySQL database at $mysql";
+    }
+    $self->{dbh} = $dbh;
 
     # set outfile
     unless ( $self->outfile ) {
@@ -306,34 +338,6 @@ sub write_content_combine {
     return ($sheet_row);
 }
 
-sub write_content_combine_obj {
-    my ( $self, $sheet, $option ) = @_;
-
-    # init table cursor
-    my $sheet_row = $option->{sheet_row};
-    my $sheet_col = $option->{sheet_col};
-    my $sql_obj   = $option->{sql_obj};     # an AlignDB::SQL object
-
-    my @combined    = @{ $option->{combined} };
-    my $combine_col = $option->{combine_col};
-
-    # this sub does not accept binding values
-
-    foreach (@combined) {
-        my @range     = @$_;
-        my $sql_clone = $sql_obj->copy;
-        $sql_clone->add_where( $combine_col => \@range );
-        my %option = (
-            sql_query  => $sql_clone->as_sql,
-            sheet_row  => $sheet_row,
-            sheet_col  => $sheet_col,
-            bind_value => $sql_clone->bind,
-        );
-        ($sheet_row) = $self->write_content_direct( $sheet, \%option );
-    }
-    return ($sheet_row);
-}
-
 sub write_content_group {
     my ( $self, $sheet, $option ) = @_;
 
@@ -375,90 +379,6 @@ sub write_content_group {
     return ($sheet_row);
 }
 
-sub write_content_group_obj {
-    my ( $self, $sheet, $option ) = @_;
-
-    # init table cursor
-    my $sheet_row = $option->{sheet_row};
-    my $sheet_col = $option->{sheet_col};
-    my $sql_obj   = $option->{sql_obj};     # an AlignDB::SQL object
-
-    my @group     = @{ $option->{group} };
-    my $group_col = $option->{group_col};
-
-    # this sub does not accept binding values and appending columns
-
-    foreach (@group) {
-        my @range     = @$_;
-        my $sql_clone = $sql_obj->copy;
-        $sql_clone->add_where( $group_col => \@range );
-        my $group_name;
-        if ( scalar @range > 1 ) {
-            $group_name = $range[0] . "--" . $range[-1];
-        }
-        else {
-            $group_name = $range[0];
-        }
-        my %option = (
-            sql_query  => $sql_clone->as_sql,
-            sheet_row  => $sheet_row,
-            sheet_col  => $sheet_col,
-            query_name => $group_name,
-            bind_value => $sql_clone->bind,
-        );
-        ($sheet_row) = $self->write_content_direct( $sheet, \%option );
-    }
-    return ($sheet_row);
-}
-
-sub write_content_indel {
-    my ( $self, $sheet, $option ) = @_;
-
-    # init objects
-    my $dbh  = $self->dbh;
-    my $fmt  = $self->format;
-    my @cols = @{ $self->columns };
-
-    # init table cursor
-    my $sheet_row = $option->{sheet_row};
-    my $sheet_col = $option->{sheet_col};
-
-    my $sql_query_1 = $option->{sql_query_1};
-    my $sql_query_2 = $option->{sql_query_2};
-    my @group       = @{ $option->{group} };
-
-    foreach (@group) {
-        my @range = @$_;
-        my $group_name;
-        if ( scalar @range > 1 ) {
-            $group_name = join "-", @range;
-        }
-        else {
-            $group_name = $range[0];
-        }
-        $sheet_row++;    # add a blank line
-        my %option = (
-            sql_query  => $sql_query_1,
-            sheet_row  => $sheet_row,
-            sheet_col  => $sheet_col,
-            query_name => $group_name,
-            bind_value => \@range,
-        );
-        ($sheet_row) = $self->write_content_direct( $sheet, \%option );
-
-        $sheet_row++;    # add a blank line
-        %option = (
-            sql_query  => $sql_query_2,
-            sheet_row  => $sheet_row,
-            sheet_col  => $sheet_col,
-            bind_value => \@range,
-        );
-
-        #print Dump \%option;
-        ($sheet_row) = $self->write_content_direct( $sheet, \%option );
-    }
-}
-
 sub write_content_series {
     my ( $self, $sheet, $option ) = @_;
 
@@ -492,58 +412,6 @@ sub write_content_series {
             bind_value => \@range,
         );
         ($sheet_row) = $self->write_content_direct( $sheet, \%option );
-    }
-}
-
-sub write_content_dd {
-    my ( $self, $sheet, $option ) = @_;
-
-    # init objects
-    my $dbh  = $self->dbh;
-    my $fmt  = $self->format;
-    my @cols = @{ $self->columns };
-
-    # init table cursor
-    my $sheet_row = $option->{sheet_row};
-    my $sheet_col = $option->{sheet_col};
-
-    my $sql_query = $option->{sql_query};
-    my @group     = @{ $option->{group} };
-
-    my $group_rows_range = int( $group[-1]->[0] + 1 ) + 2 + 3;
-
-    foreach (@group) {
-        my @range = @$_;
-        my $group_name;
-        if ( scalar @range > 1 ) {
-            $group_name = join "-", @range;
-        }
-        else {
-            $group_name = $range[0];
-        }
-        my $group_row = $sheet_row;
-        $group_row++;    # add a blank line
-        my %option = (
-            sql_query  => $sql_query,
-            sheet_row  => $group_row,
-            sheet_col  => $sheet_col,
-            query_name => $group_name,
-            bind_value => [ 'L', @range, $range[0] ],
-        );
-        ($group_row) = $self->write_content_direct( $sheet, \%option );
-
-        $group_row = $sheet_row + $group_rows_range;
-        $group_row--;
-        %option = (
-            sql_query  => $sql_query,
-            sheet_row  => $group_row,
-            sheet_col  => $sheet_col,
-            bind_value => [ 'R', @range, $range[0] ],
-            write_step => -1,
-        );
-        ($group_row) = $self->write_content_direct( $sheet, \%option );
-
-        $sheet_row += $group_rows_range;
     }
 }
 
@@ -587,160 +455,6 @@ sub write_content_highlight {
                 $row[$i], $fmt->{$style} );
         }
         $sheet_row++;
-    }
-}
-
-sub write_content_snp {
-    my ( $self, $sheet, $option ) = @_;
-
-    # init objects
-    my $dbh  = $self->dbh;
-    my $fmt  = $self->format;
-    my @cols = @{ $self->columns };
-
-    # init table cursor
-    my $sheet_row = $option->{sheet_row};
-    my $sheet_col = $option->{sheet_col};
-
-    my $sql_query1 = $option->{sql_query1};
-    my $sql_query2 = $option->{sql_query2};
-    my @base_pair  = @{ $option->{base_pair} };
-
-    for ( my $i = 0; $i < scalar @base_pair; $i++ ) {
-        my $base_pair_1 = $base_pair[$i];
-        $base_pair_1 =~ s/\W//g;
-        my $base_pair_2 = reverse $base_pair_1;
-
-        my $sub_sheet_row = $sheet_row;
-
-        my $sth = $dbh->prepare($sql_query1);
-        $sth->execute;
-
-        my %total_snp = ();
-        while ( my @row = $sth->fetchrow_array ) {
-            $total_snp{ $row[0] } = $row[1];
-        }
-
-        $sth = $dbh->prepare($sql_query2);
-        $sth->execute( $base_pair_1, $base_pair_2 );
-
-        # insert table columns
-        while ( my @row = $sth->fetchrow_array ) {
-            $sheet->write( $sub_sheet_row, 0 + $sheet_col,
-                $row[0], $fmt->{NORMAL} );
-            $sheet->write(
-                $sub_sheet_row,
-                $i + 1 + $sheet_col,
-                $row[1] / $total_snp{ $row[0] },
-                $fmt->{NORMAL}
-            );
-            $sub_sheet_row++;
-        }
-        $sth->finish;
-    }
-}
-
-sub write_content_snp2 {
-    my ( $self, $sheet, $option ) = @_;
-
-    # init objects
-    my $dbh  = $self->dbh;
-    my $fmt  = $self->format;
-    my @cols = @{ $self->columns };
-
-    # init table cursor
-    my $sheet_row = $option->{sheet_row};
-    my $sheet_col = $option->{sheet_col};
-
-    my $sql_query1 = $option->{sql_query1};
-    my $sql_query2 = $option->{sql_query2};
-    my @base_pair  = @{ $option->{base_pair} };
-
-    for ( my $i = 0; $i < scalar @base_pair; $i++ ) {
-        my $base_pair = $base_pair[$i];
-        $base_pair =~ s/\W//g;
-
-        my $sub_sheet_row = $sheet_row;
-
-        my $sth = $dbh->prepare($sql_query1);
-        $sth->execute;
-
-        my %total_snp = ();
-        while ( my @row = $sth->fetchrow_array ) {
-            $total_snp{ $row[0] } = $row[1];
-        }
-
-        $sth = $dbh->prepare($sql_query2);
-        $sth->execute($base_pair);
-
-        # insert table columns
-        while ( my @row = $sth->fetchrow_array ) {
-            $sheet->write( $sub_sheet_row, 0 + $sheet_col,
-                $row[0], $fmt->{NORMAL} );
-            $sheet->write(
-                $sub_sheet_row,
-                $i + 1 + $sheet_col,
-                $row[1] / $total_snp{ $row[0] },
-                $fmt->{NORMAL}
-            );
-            $sub_sheet_row++;
-        }
-        $sth->finish;
-    }
-}
-
-sub write_content_snp3 {
-    my ( $self, $sheet, $option ) = @_;
-
-    # init objects
-    my $dbh  = $self->dbh;
-    my $fmt  = $self->format;
-    my @cols = @{ $self->columns };
-
-    # init table cursor
-    my $sheet_row = $option->{sheet_row};
-    my $sheet_col = $option->{sheet_col};
-
-    my $sql_query1 = $option->{sql_query1};
-    my $sql_query2 = $option->{sql_query2};
-    my @base_pair  = @{ $option->{base_pair} };
-
-    for ( my $i = 0; $i < scalar @base_pair; $i++ ) {
-        my @bases           = @{ $base_pair[$i] };
-        my $base_group_name = shift @bases;
-        foreach (@bases) {
-            $_ = "\"" . $_ . "\"";
-        }
-        my $in_list = join ",", @bases;
-
-        my $sub_sheet_row = $sheet_row;
-
-        my $sth = $dbh->prepare($sql_query1);
-        $sth->execute;
-
-        my %total_snp = ();
-        while ( my @row = $sth->fetchrow_array ) {
-            $total_snp{ $row[0] } = $row[1];
-        }
-
-        my $base_query = $sql_query2;
-        $base_query =~ s/in_list/$in_list/g;
-        $sth = $dbh->prepare($base_query);
-        $sth->execute();
-
-        # insert table columns
-        while ( my @row = $sth->fetchrow_array ) {
-            $sheet->write( $sub_sheet_row, 0 + $sheet_col,
-                $row[0], $fmt->{NORMAL} );
-            $sheet->write(
-                $sub_sheet_row,
-                $i + 1 + $sheet_col,
-                $row[1] / $total_snp{ $row[0] },
-                $fmt->{NORMAL}
-            );
-            $sub_sheet_row++;
-        }
-        $sth->finish;
     }
 }
 
@@ -1017,40 +731,6 @@ sub check_column {
         return $count;
     }
 
-}
-
-sub column_ttest {
-    my ( $self, $option ) = @_;
-
-    # init objects
-    my $dbh = $self->dbh;
-
-    # bind value
-    my $bind_value = $option->{bind_value};
-    unless ( defined $bind_value ) {
-        $bind_value = [];
-    }
-
-    # init DBI query
-    my $sql_query = $option->{sql_query};
-    my $sth       = $dbh->prepare($sql_query);
-    $sth->execute(@$bind_value);
-
-    my ( @sample1, @sample2 );
-
-    # insert table columns
-    while ( my @row = $sth->fetchrow_array ) {
-        push @sample1, $row[0];
-        push @sample2, $row[1];
-    }
-
-    my $ttest = Statistics::TTest->new;
-    $ttest->set_significance(95);
-    eval { $ttest->load_data( \@sample1, \@sample2 ); };
-
-    warn $@ if $@;
-
-    return $ttest;
 }
 
 sub quantile {
